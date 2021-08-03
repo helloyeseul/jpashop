003b6f5c8cb4e791a56081816a71e7c9c80efabd
yeseul <sdc01194@gmail.com>
2021-08-03 23:51:19 +0900

    상품 엔티티 수정 - 재고 수량 변경 로직 추가

--- src/main/kotlin/jpabook/jpashop/domain/item/Item.kt
+++ src/main/kotlin/jpabook/jpashop/domain/item/Item.kt
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item
 
 import jpabook.jpashop.domain.Category
+import jpabook.jpashop.exception.NotEnoughStockException
 import javax.persistence.*
 
 @Entity
@@ -26,11 +27,30 @@ abstract class Item(name: String, price: Int, stockQuantity: Int, categories: Li
     /**
      * 재고 수량
      */
-    val stockQuantity: Int = stockQuantity
+    var stockQuantity: Int = stockQuantity
+        protected set
 
     /**
      * 카테고리 목록
      */
     @ManyToMany(mappedBy = "items")
     val categories: List<Category> = categories
+
+    /**
+     * 재고 증가
+     */
+    fun addStock(quantity: Int) {
+        this.stockQuantity += quantity
+    }
+
+    /**
+     * 재고 감소
+     */
+    fun removeStock(quantity: Int) {
+        val restStock = this.stockQuantity - quantity
+        if (restStock < 0) {
+            throw NotEnoughStockException("need more stock")
+        }
+        this.stockQuantity = restStock
+    }
 }
