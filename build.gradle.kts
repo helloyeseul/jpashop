d062bcbc9371a7f36f4ca5f8cc48c29c82dec006
yeseul <sdc01194@gmail.com>
2021-08-03 16:16:05 +0900

    쿼리 파라미터 로그 남기기 (라이브러리 사용)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.3"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.21"
-	kotlin("plugin.spring") version "1.5.21"
-	kotlin("plugin.jpa") version "1.5.21"
+    id("org.springframework.boot") version "2.5.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.21"
+    kotlin("plugin.spring") version "1.5.21"
+    kotlin("plugin.jpa") version "1.5.21"
 }
 
 group = "jpabook"
@@ -13,29 +13,33 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.junit.jupiter:junit-jupiter:5.7.0")
-	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.7.1")
+
+    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/test/kotlin/jpabook/jpashop/controller/MemberRepositoryTest.kt
+++ src/test/kotlin/jpabook/jpashop/controller/MemberRepositoryTest.kt
@@ -13,6 +13,7 @@ internal class MemberRepositoryTest {
     lateinit var memberRepository: MemberRepository
 
     @Test
+//    @Rollback(false)
     fun testMember() {
         // given
         val member = Member(username = "memberA")
@@ -24,5 +25,12 @@ internal class MemberRepositoryTest {
         // then
         assert(findMember.id == member.id)
         assert(findMember.username == member.username)
+
+        /**
+         * 같은 트랜젝션 영속성 컨텍스트 내에서 id 값이 같으면 1차 캐싱되어있던 엔티티를 사용함
+         * (find 할 때 select 쿼리조차 안날리고 캐싱되어있던 엔티티를 그대로 가져옴)
+         * 따라서 아래 두 객체는 같은 객체
+         */
+        assert(member == findMember)
     }
 }
