3b3428c6c9640e9d839684fff9565d28b8d9c258
yeseul <sdc01194@gmail.com>
2021-08-03 22:50:54 +0900

    회원 서비스 작성

--- src/main/kotlin/jpabook/jpashop/repository/MemberRepository.kt
+++ src/main/kotlin/jpabook/jpashop/repository/MemberRepository.kt
@@ -3,11 +3,9 @@ package jpabook.jpashop.repository
 import jpabook.jpashop.domain.Member
 import org.springframework.stereotype.Repository
 import javax.persistence.EntityManager
-import javax.persistence.PersistenceContext
 
 @Repository
 class MemberRepository(
-    @PersistenceContext
     private val em: EntityManager
 ) {
 
