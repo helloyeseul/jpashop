44e44138d524ad9a86da6b06ae6edee7563cd6a8
yeseul <sdc01194@gmail.com>
2021-08-05 04:19:27 +0900

    주문 검색 레포지토리, 서비스 작성

---
+++ src/main/kotlin/jpabook/jpashop/repository/OrderSearch.kt
@@ -0,0 +1,8 @@
+package jpabook.jpashop.repository
+
+import jpabook.jpashop.domain.OrderStatus
+
+data class OrderSearch(
+    val memberName: String? = null,
+    val orderStatus: OrderStatus? = null
+)
